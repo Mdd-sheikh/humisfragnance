f3d5b994de3229045a04fc2f565d17bf259c5928
Your Name <your-email@example.com>
2026-08-17 09:42:19 -0700

    fix

--- admin/admin/src/context/Context.jsx
+++ admin/admin/src/context/Context.jsx
@@ -5,7 +5,7 @@ export const Context = createContext(null)
 
 const Contextprovider = ({ children }) => {
 
-    const API_URL = "https://humisfragnance-3.onrender.com/api";
+    const API_URL = "https://humisfragnance-3.onrender.com";
 
     const value = {
      API_URL,
