f6fc748fed286ec302e2720c4f6dff6ce45bb545
Your Name <your-email@example.com>
2026-08-17 22:25:46 -0700

    remove dublicate file

--- humi/src/routes/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PublicRoute = () => {
-    const token = localStorage.getItem("token");
-
-    return token ? <Navigate to="/" replace /> : <Outlet />;
-};
-
-export default PublicRoute;
